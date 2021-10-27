926b92915e65951316fe890ed497b722342d38d1
s00188563 <fernandezluigi10@yahoo.ie>
2021-10-27 17:30:39 +0100

    database schema done

---
+++ models/vehicleModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema(
+  {
+    vehicle_id: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: Object,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+module.exports = Vehicle;
